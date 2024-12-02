407318c52c2b581de2ecd9ba070a1b8ccc0d2245
LENOVO-PC <amirulmukminin0565@gmail.com>
2024-12-02 16:36:07 +0700

    KabupatenKotaRiau

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") } // Tambahkan JitPack untuk plugin jika diperlukan
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Tambahkan JitPack untuk dependency project
+    }
+}
+
+rootProject.name = "KabupatenKotaRiauApp"
+include(":app")
